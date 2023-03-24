8014e22877f4ef0eca75fd61c03c06c3df146f1c
XujuanChen <xchen022@citymail.cuny.edu>
2023-03-24 02:50:01 +0000

    step6

--- src/Components/sideNav.jsx
+++ src/Components/sideNav.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CryptoScam from "./cryptoScam"
 
 const SideNav = () => (
   <div className="sidenav">
     <h1>here is my side nav bar</h1>
+    <CryptoScam />
   </div>
 );
 
